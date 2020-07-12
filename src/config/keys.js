1a028135673401700633bfd059bacfabbdd8ddf0
Daniel Brito <danielmanfred@gmail.com>
2020-07-12 14:26:17 -0300

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
     jwt: {
-        secret: 'jx0tbchs20y4fsu6sb7y', // Code to verify signature
+        secret: 'xxxxxxxxxxx', // Code to verify signature
         session: { session: false }
     },
     mongodb: { // Database hosted on Mlab
-        mlab: 'mongodb://manfred:ellav19@ds211875.mlab.com:11875/valle'
+        mlab: 'mongodb://manfred:xxxxx@xxxxx.mlab.com:11875/valle'
     },
     server: { // Setting port
         port: process.env.PORT || 8081
